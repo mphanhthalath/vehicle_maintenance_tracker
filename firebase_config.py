01c14e7a61826952275c487f6a9df98977ebe504
mphanhthalath <76753348+mphanhthalath@users.noreply.github.com>
2024-10-30 16:59:10 -0500

    update

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,22 @@
-import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 import streamlit as st
-import json
 
-# Access Firebase credentials from Streamlit secrets
-cred_dict = st.secrets["firebase"]
+# Load Firebase credentials from Streamlit secrets
+cred_dict = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+}
 
-# Initialize Firebase
+# Initialize Firebase only if it hasn’t been initialized
 if not firebase_admin._apps:
     cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
